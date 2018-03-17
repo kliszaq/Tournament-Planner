00b863f15e61b2ba2031ed6077d6484456e47bea
kliszaq <kamil.kliczbor@gmail.com>
2018-03-17 23:02:35 +0100

    1. Dodanie lokalnego serwerta -> http://127.0.0.1:8000 komenda php bin/console server:run z konsoli.
    2. Przeformatowanie HelloController.php

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,18 +1,18 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController{
+class HelloController
+{
     /**
      * @Route("/hello")
      */
     public function helloWorld()
     {
-return new Response(
-    '<html><body>Symfony says: hello world!</body></html>'
-);
+        return new Response(
+            '<html><body>Symfony says: hello world!</body></html>'
+        );
     }
 }
-?>
